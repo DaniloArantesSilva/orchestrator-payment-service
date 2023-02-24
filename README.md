528b1330d1afaffd6a4e8592793e16beffa93c6b
Danilo Arantes <daniloarantes_00@hotmail.com>
2023-02-24 15:15:07 -0300

    second commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# orchestrator-payment-service
---
+++ src/main/java/com/arantes/payment/application/core/domain/enums/SaleEvent.java
@@ -0,0 +1,9 @@
+package com.arantes.payment.application.core.domain.enums;
+
+public enum SaleEvent {
+
+    EXECUTE_PAYMENT,
+    PAYMENT_EXECUTED,
+    PAYMENT_FAILED
+
+}
